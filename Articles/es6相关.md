4783cb73b50da05e3761fd818d1b982db013b787
JimmyWithOutOnion <jimmyjyc24@gmail.com>
2020-01-21 09:59:51 +0800

    leetcode 笔记整理

--- Articles/es6相关.md
+++ Articles/es6相关.md
@@ -1,3 +1,4 @@
+资料来源：http://es6.ruanyifeng.com/#docs/destructuring
 # 表达式
 ## 声明
 - const 命令：声明变量
@@ -22,10 +23,57 @@
 1. 不允许重复声明
 2. 未定义就使用会报错：const 命令 和 let 命令 不存在变量提升
 3. 暂时性死区：在代码块内使用 let 命令 声明变量之前，该变量都不可用
+4. const 保证的并不是值不得改动，而是变量指向的那个内存地址所保存的数据不得改动
+对于简单类型的数据，值就保存在变量指向的内存地址
+但对于复合类型但数据，保存的只是指向实际数据的指针，const只能保证指针固定，至于它指向的数据结构是不是可变的，就完全不能控制了
+```javascript
+const foo = {};
 
-## 结构赋值
+// 为 foo 添加一个属性，可以成功
+foo.prop = 123;
+foo.prop // 123
+
+// 将 foo 指向另一个对象，就会报错
+foo = {}; // TypeError: "foo" is read-only Assignment to constant variable.
+
+const a = [];
+a.push('Hello'); // 可执行
+a.length = 0;    // 可执行
+a = ['Dave'];    // 报错
+```
+5. 顶层对象
+ES5 之中，顶层对象的属性与全局变量是等价的，这样的设计有很大的问题
+-首先是没法在编译时就报出变量未声明的错误
+-其次，很容易不知不觉就创建了全局变量
+-不利于模块化编程
 
 
+## 解构赋值
+- 字符串解构： 
+``` javascript
+const [a, b, c, d, e] = 'hello'
+```
+类似数组的对象都有一个 length 属性，因此还可以对这个属性解构赋值
+``` javascript
+let { length: len } = 'hello'
+console.log(len) // 5
+```
+
+- 数值和布尔值解构：
+解构赋值时，如果等号右边是数值和布尔值，则会先转为对象
+
+``` javascript
+const { toString: s } = 123
+s === Number.prototype.toString // true
+
+
+const { toString: s } = true
+s === Boolean.prototype.toString // true
+```
+上面代码中，数值和布尔值对包装对象都有 toString 属性，因此变量s都能取到值
+解构赋值的规则是，只要等号右边的值不是对象或数组，就先将其转化为对象，由于 undefined 和 null 无法转化为对象，所以对他们进行解构赋值，都会报错
+
+- 对象解构
 
 
 
--- Articles/leetcode.js
+++ Articles/leetcode.js
@@ -1,57 +1,57 @@
 // 1. 两数只和
-var twoSum = function(nums, target) {
+var twoSum = function (nums, target) {
     let myMap = new Map();
     for (let i = 0; i < nums.length; i++) {
-      myMap.set(nums[i], i);
+        myMap.set(nums[i], i);
     }
-    
+
     for (let i = 0; i < nums.length; i++) {
-      let diff = target - nums[i];
-      let result = myMap.get(diff);
-      if (result && result !== i) {
-        return [i, result];
-      }
+        let diff = target - nums[i];
+        let result = myMap.get(diff);
+        if (result && result !== i) {
+            return [i, result];
+        }
     }
-  }
-  
-  // 8. 字符串转换整数
-  var myAtoi = function(str) {
+}
+
+// 8. 字符串转换整数
+var myAtoi = function (str) {
     str.trim();
     if (typeof str[0] != "number" || str[0] !== "-") {
-      return 0
+        return 0
     }
     for (let i = 0; i < str.length; i++) {
-      if (typeof str[i] == "number") {
-        
-      }
+        if (typeof str[i] == "number") {
+
+        }
     }
-  };
-  
-  // 20. 
-  var isValid = function(s) {
+};
+
+// 20. 
+var isValid = function (s) {
     if (s.length % 2 != 0) {
-      return false;
+        return false;
     }
-    let match = {'(': ')', '{': '}', '[': ']'};
+    let match = { '(': ')', '{': '}', '[': ']' };
     let stack = [];
     for (let i = 0; i < s.length; i++) {
-      if (stack.length === 0) {
-        stack.push(s[i]);
-      } else {
-        if (match[stack[stack.length - 1]] === s[i]) {
-          stack.pop();
+        if (stack.length === 0) {
+            stack.push(s[i]);
         } else {
-          stack.push(s[i]);
+            if (match[stack[stack.length - 1]] === s[i]) {
+                stack.pop();
+            } else {
+                stack.push(s[i]);
+            }
         }
-      }
     }
     return stack.length === 0 ? true : false;
-  };
-  
-  // 21. 合并两个有序链表
-  var mergeTwoLists = function(l1, l2) {
+};
+
+// 21. 合并两个有序链表
+var mergeTwoLists = function (l1, l2) {
     if (l1 == null) {
-      return l2;
+        return l2;
     } else if (l2 == null) {
         return l1;
     } else if (l1.val < l2.val) {
@@ -61,8 +61,68 @@ var twoSum = function(nums, target) {
         l2.next = mergeTwoLists(l1, l2.next);
         return l2;
     }
-  };
-  
-  
-  
-  
+};
+// 58 最后一个单词的长度
+var lengthOfLastWord = function (s) {
+    let trimedString = s.trim()
+    let count = 0
+    let index = trimedString.length - 1
+    while (index >= 0 && trimedString[index] !== ' ') {
+        count += 1
+        index -= 1
+    }
+    return count
+};
+
+// 105 从前序和中序遍历序列构造二叉树
+
+
+
+// 217 存在重复元素
+var containsDuplicate = function (nums) {
+    let bracket = {}
+    for (let i = 0; i < nums.length; i++) {
+        if (bracket[nums[i]]) {
+
+            return true
+        } else {
+            bracket[nums[i]] = 1
+        }
+    }
+    return false
+};
+
+// 206 反转链表
+var reverseList = function (head) {
+
+    let pre = null, current = head, post = null
+    while (current) {
+        post = current.next
+        current.next = pre
+
+        pre = current
+        current = post
+    }
+    return pre
+};
+
+// 240 搜索二维矩阵2
+var searchMatrix = function (matrix, target) {
+
+    if (matrix.length > 0 && matrix[0].length > 0) {
+        let row = 0
+        let col = matrix[0].length - 1
+        while (row < matrix.length && col >= 0) {
+            if (target > matrix[row][col]) {
+                row += 1
+            } else if (target < matrix[row][col]) {
+                col -= 1
+            } else {
+                return true
+            }
+        }
+    }
+    return false
+};
+
+
